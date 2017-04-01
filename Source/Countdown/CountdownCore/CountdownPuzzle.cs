b99581c0df4ac698a71899a8f261397f59d9c9dc
lee morton <leemorton123@gmail.com>
2017-04-01 11:38:40 +0100

    Created abstract solver classes

---
+++ Source/Countdown/CountdownCore/CountdownPuzzle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountdownCore
+{
+    class CountdownPuzzle
+    {
+        public CountdownPuzzle(int solution, List<int> numbers)
+        {
+            this.solution = solution;
+            this.numbers = numbers;
+            this.size = numbers.Count;
+        }
+
+        private List<int> numbers {  get; set;}
+        public int solution { get; private set; }
+        public int size { get; private set; }
+
+
+        public int GetNumber(int index)
+        {
+            return numbers[index];
+        }
+    }
+}
---
+++ Source/Countdown/CountdownCore/CountdownSolver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountdownCore
+{
+    abstract class CountdownSolverBase
+    {
+
+        CountdownSolverMethod method;
+        CountdownSolverContainer container;
+        CountdownPuzzle puzzle;
+        
+    }
+
+    abstract class CountdownSolverMethod
+    {
+        
+        public abstract void Initialise(CountdownSolverContainer container, CountdownPuzzle puzzle);
+
+    }
+
+    abstract class CountdownSolverContainer
+    {
+
+
+        /// <summary>
+        /// Initialises the container to the 'first' iteration by populating the container with the intitial numbers
+        /// in the puzzle
+        /// </summary>
+        /// <param name="puzzle"></param>
+        public abstract void Initialise(CountdownPuzzle puzzle);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>hashes that have been computed so far</returns>
+        public abstract List<int> GetHashes();
+        /// <summary>
+        /// Returns a list of countdownNumber corresponding to the supplied hash.
+        /// </summary>
+        /// <param name="hash">hash representing the combination of numbers used to make the puzzle</param>
+        /// <returns>List of CountdownNumber</returns>
+        public abstract List<CountdownNumber> GetNumbers(int hash);
+    }
+}
